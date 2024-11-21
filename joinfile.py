5e469d2c101b17ff033a32c74c015facc1c378b6
zzuse <zzuse@outlook.com>
2024-11-21 04:52:28 -0600

    fix bug

--- joinfile.py
+++ joinfile.py
@@ -14,26 +14,23 @@ dirFiles5 = dirFiles[2220:2750]
 dirFiles6 = dirFiles[2750:]
 
 
-total_words = 0
 if __name__ == '__main__':
     for i in range(1, 7):
+        total_words = 0
         subdirFiles = "dirFiles" + str(i)
-        print(i, subdirFiles)
         with open('join{}.txt'.format(i), 'a+', encoding='utf-8') as f:
             for file in eval(subdirFiles):
-                print(file)
+                print(file, end=" ")
                 with open("./zz/{}".format(file), 'r', encoding='utf-8') as j:
                     f.write(os.path.splitext(file)[0])
                     f.write('\n\n')
                     for line in j:
-                        line.replace(u'\u3000', '')
+                        line = line.replace(u'\u3000', u'')
                         s1 = line.strip()
+                        f.write(line)
                         for ca in s1:
-                            if ca == u'\u3000':
-                                continue
-                            else:
-                                f.write(ca)
-                                total_words = total_words + 1
-                                # print(hex(ord(ca)), ca)
+                            total_words = total_words + 1
+                            # print(hex(ord(ca)), ca)
                     f.write('\n\n')
-    print("total words ", total_words)
+            print()
+        print(subdirFiles, " have total words ", total_words, end='\n')
