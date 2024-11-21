4ff9272eea835c730182846dd1de6959f754536a
zzuse <zzuse@outlook.com>
2024-11-20 23:56:24 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# downloadtxt A web novel scraper
---
+++ download.py
@@ -0,0 +1,53 @@
+import requests
+import re
+from lxml import etree
+import time
+import cloudscraper
+
+
+urls = ['https://m.ddtxt8.cc/book/73206/{}.html'.format(i) for i in range(1, 3)]
+uri = r'https://m.ddtxt8.cc'
+book = r'/book/73206/'
+# urls = ['https://m.ddtxt8.cc/book/73206/{}.html'.format(i) for i in range(1, 3257)]
+path = r'/Users/zhangzhen/Documents/Code/Self/downloadtxt/zz/'
+
+scraper = cloudscraper.create_scraper()
+
+def get_text(local_url):
+    # r = requests.get(local_url)
+    r = scraper.get(local_url)
+    r.encoding = 'utf-8'
+    selector = etree.HTML(r.text)
+    title = selector.xpath('/html/head/title/text()')
+    real_title = ''.join((ch if ch in '0123456789.-e' else '') for ch in title[0])
+    print(real_title)
+
+    text = selector.xpath('//*[@id="chaptercontent"]/text()')
+    # remove 请收藏：https://m.ddtxt8.cc
+    # replace two space to new line
+    with open(path+real_title+'.txt', 'a+', encoding='utf-8') as f:
+        for i in text:
+            s1 = re.sub("请收藏：https://m.ddtxt8.cc", "", i)
+            f.write(s1)
+    n_url = selector.xpath('//a/@href')
+    print(n_url)
+    time.sleep(1)
+    return n_url[4]
+
+
+if __name__ == '__main__':
+    for page_num in range(4, 3257):
+        url = uri + book + str(page_num) + '.html'
+        print(url)
+        next_page = get_text(url)
+        print(next_page)
+        next_number = re.sub('_.?.html$', '', next_page.split('/')[3])
+        print(next_number)
+        print('page_num {}'.format(page_num))
+        while next_number == str(page_num):
+            print('should get page {}'.format(uri+next_page))
+            next_page = get_text(uri + next_page)
+            print(next_page)
+            next_number = re.sub('_.?.html$', '', next_page.split('/')[3])
+            print(next_number)
+            print('page_num {}'.format(page_num))
